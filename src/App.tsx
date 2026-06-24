8454804d747c14a5b5137fbeabe58938a55fdf59
Santhosh-Rangaraj <santhoshvav@gmail.com>
2026-06-25 00:01:44 +0530

    work on callback

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,31 @@ import NotepadHeader from "./Components/Notepad_header";
 import AddNote from "./Components/AddNote";
 import Footer from './/Components/Footer'
 import NotesSection from "./Components/NotesSection";
+import { useState } from "react";
+
+type Note = {
+  id: string;
+  title: string;
+  content: string;
+  date: string;
+};
 
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+  console.log('notes: ', notes);
+
   return (
-    <>
+    <div className="app-container">
           <NotepadHeader />  
           <main>
             <section className="main-container">
-            <AddNote />
+            <AddNote onAddNote={(noteData) => setNotes([...notes, noteData])} />
             <NotesSection />
             </section>
           </main>
           <Footer/>
-    </>
+    </div>
   );
 }
 
--- src/Components/AddNote.tsx
+++ src/Components/AddNote.tsx
@@ -1,16 +1,39 @@
 import react from "react";
 import "../styles/add_note.css";
+import { useState } from "react";
 
-const AddNote = () => {
+interface AddNoteProps {
+  onAddNote: (noteData: { id: string; title: string; content: string; date: string }) => void;
+}
+
+const AddNote = ({onAddNote}: AddNoteProps) => {
+  const [title, setTitle] = react.useState("");
+  const [content, setContent] = react.useState("");
+
+const today = new Date().toLocaleDateString();
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+
+    const noteData={
+       id: Math.random().toString(36).substr(2, 9),
+      title: title,
+      content: content,
+      date: today
+    }
+    onAddNote(noteData);
+    setTitle("");
+    setContent("");
+  };
+
   return (
     <div className="add-note-container">
       <h3>Add Note</h3>
-      <form className="note-form">
+      <form className="note-form" onSubmit={handleSubmit}>
         <div>
-          <input type="text" id="title" name="title"  placeholder="Note Title"/>
+          <input type="text" id="title" name="title"  placeholder="Note Title" value={title} onChange={(e) => setTitle(e.target.value)} />
         </div>
         <div>
-          <textarea id="content" name="content"  placeholder="Write your note here..." rows={12}/>
+          <textarea id="content" name="content"  placeholder="Write your note here..." rows={12} value={content} onChange={(e) => setContent(e.target.value)}/>
         </div>
         <button type="submit">Add Note</button>
       </form>
