92c55a6b6329095a6ec2b6b0897aea55dd99ef2f
Santhosh-Rangaraj <santhoshvav@gmail.com>
2026-07-07 23:27:45 +0530

    feat: Add EmptyNotes component and enhance styling; improve notes list header and empty state presentation

--- src/Components/NotesList.tsx
+++ src/Components/NotesList.tsx
@@ -2,13 +2,28 @@ import React from "react";
 import "../styles/NotesList.css";
 import Cards from "./Cards";
 import {type Note} from '../App'
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+
+const EmptyNotes=()=>{
+  return (
+  <div className="empty-notes-container">
+    <DescriptionOutlinedIcon className="empty-notes-icon"/>
+    <div className="empty-notes-heading">
+      <h4>Notes Not Found...</h4>
+      <p>Create your first note or try a different search.</p>
+    </div>
+  </div>
+  )
+}
 
 const NotesList = ({notes, onClickedNote,onEditData}:{notes:Note[], onClickedNote:(note: Note) => void, onEditData: (note: Note) => void}) => {
   return (
     <div className="notes-list-container">
-      <h3>Your Notes</h3>
+    <div className="notes-list-header">
+      <h3>Your Notes</h3><p>{notes.length}</p>
+    </div>
     <div className="notes-list">
-      {notes.length===0? (<p style={{fontSize:"1.5rem", "textAlign":"center",width:"100%"}}>There is no notes to view ...</p>):(notes.map((note) => (
+      {notes.length===0? (<EmptyNotes></EmptyNotes>):(notes.map((note) => (
         <Cards onClickedNote={onClickedNote} onEditData={onEditData} key={note.id} note={note} />
       )))}
     </div>
